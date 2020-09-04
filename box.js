999d9df7db24b14c564e23b71a4b82c0f97a3566
annaasmi <68715188+annaasmi@users.noreply.github.com>
2020-09-03 20:28:41 -0400

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0,
+        'friction':0,
+        'density':6.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.Visiblity = 255;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    if(this.body.speed < 3){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke("lightblue");
+    fill("lightblue");
+    rect(0, 0, this.width, this.height);
+    pop();
+    }else{
+      text("I'm amazing",100,100);
+      World.remove(world, this.body);
+      push();
+      this.Visiblity = this.Visiblity - 5;
+      pop();
+    }
+  }
+};
